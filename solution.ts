ccc6adc6b702faf62d47610ade1a06f7d68ba0b2
arafat22184 <123alarafat@gmail.com>
2025-11-16 23:08:05 +0600

    fix: Problem-2 solved

--- solution.ts
+++ solution.ts
@@ -22,3 +22,18 @@ const getLength = (value: string | any[]) => {
     return value.length
   }
 }
+
+
+// ! Problem-3
+class Person {
+  name: string;
+  age: number;
+  constructor(name:string, age:number) {
+    this.name = name,
+    this.age = age;
+  }
+
+  getDetails() {
+    return `'Name: ${this.name}, Age: ${this.age}'`
+  }
+}
